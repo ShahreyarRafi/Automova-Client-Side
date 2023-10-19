c23d6c4744ef0e07a49bc2ba0790ce1bf99d8705
ShahreyarRafi <shahreyar.rafi@gmail.com>
2023-10-19 19:59:49 +0600

    added updateProduct component

--- src/Pages/AddProductPage/AddProductPage.jsx
+++ src/Pages/AddProductPage/AddProductPage.jsx
@@ -5,7 +5,6 @@ import AddProducts from '../../Components/AddProducts/AddProducts';
 const AddProductPage = () => {
     return (
         <div>
-            
             <AddProducts></AddProducts>
         </div>
     );
